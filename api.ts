a49cc466873f54152b8bc349b4b3d9793bcb09b7
Jennifer Bland <ratracegrad@gmail.com>
2023-12-28 13:51:17 -0500

    Deploy

--- api.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import serverless from "serverless-http";
-import indexRouter from "./routes/index";
-
-const api = express();
-
-api.use('/', indexRouter);
-
-export const handler = serverless(api);
